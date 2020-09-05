470e7e0f0336e11f7c502d1dcc54d79df14cc2ba
Tobias Madsen <tobi8678@cvkweb.dk>
2020-09-05 16:04:17 +0200

    Add .env to gitignore

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const db = require("../lib/config/database");
 require("dotenv").config();
-
 const app = express();
 
+
+app.use(function(req,res,next){
+    db();
+    next();
+});
 const port = process.env.PORT;
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
