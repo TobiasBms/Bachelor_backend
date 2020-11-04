04e1fb0d0a8283a452f23ab5e3abc88e16213b71
TobiasBms <tobi8678@edu.ucl.dk>
2020-11-04 19:05:16 +0100

    RestaurantCategory route, Modal and Controller

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const {db} = require('../lib/config/database');
 const restaurantRouteGroup = require('./routes/restaurant');
 
 app.use(function(req,res,next){
+
+    //Establish the connection for the database
     db();
     next();
 });
@@ -14,7 +16,9 @@ app.use(express.json());
 app.use("/api", restaurantRouteGroup); 
 
 const port = process.env.PORT;
+const baseurl = process.env.BASEURL;
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+app.listen(port, () => { 
+    console.log(`Example app listening at ${baseurl}:${port}`)
 })
+
--- src/routes/restaurant/index.js
+++ src/routes/restaurant/index.js
@@ -6,4 +6,8 @@ restaurantRouteGroup
     .get("/restaurant", restaurantController.getMany)
     .get("/restaurant/:id", restaurantController.getOne);
 
+restaurantCategoryRouteGroup
+    .get('restaurantCategory', restaurantCategoryRouteGroup.getMany)
+    .get('restaurantCategory/:id', restaurantCategoryRouteGroup.getOne);
+
 module.exports = restaurantRouteGroup;
