16356e52abaea95f426b6ba66d65a256a20f0245
lrpe <lassep@protonmail.com>
2020-11-08 07:36:11 +0100

    Removed commented out property

--- src/db/models/restaurant.js
+++ src/db/models/restaurant.js
@@ -3,7 +3,6 @@ const { DataTypes } = require("sequelize");
 module.exports = (sequelize) => {
   sequelize.define('Restaurant', {
     name: DataTypes.STRING,
-    // zip_code: DataTypes.CHAR,
     zip_code: {
       type: DataTypes.CHAR,
       references: {
