338c90ce2aa1f8879a051bc8a6698ffd00a31ac2
TobiasBms <tobi8678@edu.ucl.dk>
2020-11-09 14:34:54 +0100

    Full crud on privilege

--- src/http/routes/privilege.js
+++ src/http/routes/privilege.js
@@ -1,4 +1,5 @@
 const {models, model} = require('../../db');
+const privilege = require('../../db/models/privilege');
 const { getIdParam } = require('../utils');
 
 
@@ -21,7 +22,56 @@ async function getById(req,res) {
     }
 }
 
+async function create(req,res){
+    try{
+
+        const privilege = await models.Privilege.create(req.body);
+        res.send(200, privilege);
+    }catch(error){
+        res.send(401, {
+            message: error.message
+        })
+    }
+}
+
+async function update(req,res){
+    try{
+        const id = getIdParam(req);
+
+        const privilege = await models.Privilege.update(req.body, {
+            where:  {
+                id: id
+            }
+        });
+
+        res.send(200, privilege);
+    }catch(error){
+
+    }
+}
+
+async function remove(req,res){
+    try{
+        const id = getIdParam(req);
+        const privilege = await models.Privilege.destroy({
+            where: {
+                id: id
+            }
+        })
+
+        res.send(200, privilege);
+        
+    }catch(error){
+        res.send(401, {
+            message: error.message
+        })
+    }
+}
+
 module.exports = {
     getAll,
-    getById
+    getById,
+    create,
+    remove,
+    update
 }
