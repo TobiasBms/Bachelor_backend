8bdab4e1f78e7ee4580be562f0df9d3575e43c09
lrpe <lassep@protonmail.com>
2020-11-16 23:30:09 +0100

    Refactored restaurant controller

--- src/http/routes/restaurant.js
+++ src/http/routes/restaurant.js
@@ -1,5 +1,6 @@
 const { models } = require('../../db')
 const restaurantService = require('../../services/restaurant.service')
+const { NotFoundError, BadRequestError } = require('restify-errors')
 var Router = require('restify-router').Router
 var router = new Router()
 const { getIdParam } = require('../middleware/id')
@@ -21,77 +22,62 @@ async function getAll(_req, res, next) {
   }
 }
 
-async function getById(req, res) {
+async function getById(req, res, next) {
   try {
-    const restaurant = await models.Restaurant.findByPk(req.id, {
-      include: [
-        { model: models.City, as: 'city' },
-        {
-          model: models.RestaurantCategory,
-          as: 'categories',
-          through: { attributes: [] },
-        },
-        {
-          model: models.RestaurantHours,
-          as: 'hours',
-          attributes: { exclude: ['restaurant_id'] },
-        },
-      ],
-      attributes: { exclude: ['zip_code'] },
-    })
-    if (restaurant) {
-      res.send(200, restaurant)
+    const restaurant = await restaurantService.getById(req.id)
+    if (restaurant === null) {
+      next(new NotFoundError('This restaurant does not exist in our database.'))
     } else {
-      res.send(404, {
-        message: 'This restaurant does not exist in our database.',
-      })
+      res.send(200, restaurant)
+      next()
     }
   } catch (error) {
-    res.send(400, { message: error.message })
+    next(error)
   }
 }
 
-async function create(req, res) {
+async function create(req, res, next) {
   try {
     if (req.body.id) {
-      res.send(400, {
-        message:
-          'ID should not be provided, since it is determined automatically by the database.',
-      })
+      next(
+        new BadRequestError(
+          'ID should not be provided, since it is automatically generated.'
+        )
+      )
     } else {
-      const restaurant = await models.Restaurant.create(req.body)
+      const restaurant = await restaurantService.create(req.body)
       res.send(201, restaurant)
+      next()
     }
   } catch (error) {
-    res.send(400, {
-      message: error.message,
-    })
+    next(error)
   }
 }
 
-async function update(req, res) {
-  // We only accept an UPDATE request if the `:id` param matches the body `id`
-  if (req.body.id === req.id) {
-    await models.Restaurant.update(req.body, {
-      where: {
-        id: req.id,
-      },
-    })
-    res.status(200).end()
-  } else {
-    res
-      .status(400)
-      .send(
-        `Bad request: param ID (${req.id}) does not match body ID (${req.body.id}).`
-      )
+async function update(req, res, next) {
+  try {
+    if (req.body.id !== req.id) {
+      next(new BadRequestError('ID parameter does not match body.'))
+    } else {
+      await models.Restaurant.update(req.body, {
+        where: { id: req.id },
+      })
+      res.send(204)
+      next()
+    }
+  } catch (error) {
+    next(error)
   }
 }
 
-async function remove(req, res) {
-  await models.Restaurant.destroy({
-    where: {
-      id: req.id,
-    },
-  })
-  res.status(200).end()
+async function remove(req, res, next) {
+  try {
+    await models.Restaurant.destroy({
+      where: { id: req.id },
+    })
+    res.send(204)
+    next()
+  } catch (error) {
+    next(error)
+  }
 }
