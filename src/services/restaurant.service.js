333a1115e690cd00bdd1e0165a51ced22b466d6f
lrpe <lassep@protonmail.com>
2020-11-16 19:56:38 +0100

    Added simple Restaurant service

---
+++ src/services/restaurant.service.js
@@ -0,0 +1,29 @@
+const { models } = require('../../db')
+
+module.exports = {
+  getAll,
+  getById,
+  create,
+  update,
+  remove,
+}
+
+async function getAll() {
+  return await models.Restaurant.findAll()
+}
+
+async function getById(id) {
+  return await models.Restaurant.findByPk(id)
+}
+
+async function create(body) {
+  return await models.Restaurant.create(body)
+}
+
+async function update(id, body) {
+  await models.Restaurant.update(body, { where: { id } })
+}
+
+async function remove(id) {
+  await models.Restaurant.destroy({ where: { id } })
+}
