3cca4ff828ea5cb207ea33e68bb3d6a3918efc10
lrpe <lassep@protonmail.com>
2020-11-17 18:17:03 +0100

    Added privileges scope on manager

--- src/db/scopes/manager.js
+++ src/db/scopes/manager.js
@@ -1,5 +1,5 @@
 module.exports = function applyScopes(sequelize) {
-  const { Manager, ManagerRole } = sequelize.models
+  const { Manager, ManagerRole, Privilege } = sequelize.models
 
   // Include manager role
   Manager.addScope("role", {
@@ -13,5 +13,17 @@ module.exports = function applyScopes(sequelize) {
     ],
     // Exclude role ID on manager as it's included in role object
     attributes: { exclude: ["role_id"] },
+  })
+
+  // Include extended privileges
+  Manager.addScope("privileges", {
+    include: [
+      {
+        model: Privilege,
+        as: "privileges",
+        // Avoid adding join table as child of privilege objects
+        through: { attributes: [] },
+      },
+    ],
   })
 }
