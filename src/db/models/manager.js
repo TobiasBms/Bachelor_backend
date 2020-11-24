4aa2466f373e0b28ae364e441a193a995853ee0d
lrpe <lassep@protonmail.com>
2020-11-24 11:38:55 +0100

    Exclude password from manager by default

--- src/db/models/manager.js
+++ src/db/models/manager.js
@@ -1,23 +1,34 @@
 const { DataTypes } = require("sequelize")
 
 module.exports = sequelize => {
-  sequelize.define("Manager", {
-    restaurant_id: {
-      type: DataTypes.NUMBER,
-      references: {
-        model: sequelize.models.Restaurant,
-        key: "id",
+  sequelize.define(
+    "Manager",
+    {
+      restaurant_id: {
+        type: DataTypes.NUMBER,
+        references: {
+          model: sequelize.models.Restaurant,
+          key: "id",
+        },
       },
+      role: DataTypes.ENUM({
+        values: ["Admin", "Manager", "Waiter"],
+      }),
+      first_name: DataTypes.STRING,
+      last_name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      phone: DataTypes.CHAR,
+      password: DataTypes.STRING,
+      registered_at: DataTypes.DATEONLY,
+      last_login: DataTypes.DATE,
     },
-    role: DataTypes.ENUM({
-      values: ["Admin", "Manager", "Waiter"],
-    }),
-    first_name: DataTypes.STRING,
-    last_name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    phone: DataTypes.CHAR,
-    password: DataTypes.STRING,
-    registered_at: DataTypes.DATEONLY,
-    last_login: DataTypes.DATE,
-  })
+    {
+      defaultScope: {
+        attributes: { exclude: ["password"] },
+      },
+      scopes: {
+        withPassword: { attributes: {} },
+      },
+    }
+  )
 }
