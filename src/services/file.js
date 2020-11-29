ac52beee519919bc7874a1066b9146e7f2a949e7
lrpe <lassep@protonmail.com>
2020-11-29 14:38:09 +0100

    File upload service

--- src/services/file.js
+++ src/services/file.js
@@ -1,4 +1,9 @@
 const { File, RestaurantHasFile } = require("../db").models
+const fs = require("fs").promises
+const crypto = require("crypto")
+require("dotenv").config()
+
+const allowedFiles = ["image/jpeg", "image/png", "image/gif"]
 
 module.exports = { getAll, create }
 
@@ -8,7 +13,30 @@ async function getAll(restaurantId) {
 
 async function create(restaurantId, multipartBody) {
   const fileData = multipartBody.file
-  const file = await File.create({ name: fileData.name, hash: "test" })
+  if (!allowedFiles.includes(fileData.type)) {
+    throw new Error("Filetype is not allowed")
+  }
+
+  /* Hash filename and store in filesystem */
+  const hash = getHash(fileData.name)
+  const destination = await getFileLocation(hash)
+  await fs.rename(fileData.path, `${destination}.jpg`)
+
+  /* Save file info to database */
+  const file = await File.create({ name: fileData.name, hash })
   await RestaurantHasFile.create({ restaurantId, fileId: file.id })
   return file
+}
+
+function getHash(filename) {
+  const withDate = filename + Date.now()
+  return crypto.createHash("md5").update(withDate).digest("hex")
+}
+
+async function getFileLocation(hash) {
+  let destination = process.env.UPLOAD_DIR
+  destination += `${hash.split("", 4).join("/")}/`
+  await fs.mkdir(destination, { recursive: true })
+  destination += `${hash.slice(4)}`
+  return destination
 }
