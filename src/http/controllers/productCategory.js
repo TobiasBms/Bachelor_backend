202f61454c6abc19cd566529166d38b11227d317
TobiasBms <tobi8678@edu.ucl.dk>
2020-11-30 10:37:25 +0100

    made business logic for the productCategory

--- src/http/controllers/productCategory.js
+++ src/http/controllers/productCategory.js
@@ -1,10 +1,10 @@
 const { Router } = require("restify-router")
 const { noId } = require("../../../config/errors.json")
-const productService = require("../../services/productCategory")
+const categoryService = require("../../services/productCategory")
 const { getIdParam, authorize } = require("../middleware")
 const getScopesQuery = require("../middleware/scopes")
 const router = new Router()
-const { BadRequestError } = require("restify")
+const { BadRequestError, NotFoundError } = require("restify")
 router.post("", authorize(), create)
 router.get("", authorize(), getScopesQuery, getAll)
 router.get("/:id", authorize(), getIdParam, getById)
@@ -15,7 +15,7 @@ module.exports = router
 
 async function getAll(req, res, next) {
   try {
-    const productCategories = await productService.getAll(req.scopes)
+    const productCategories = await categoryService.getAll(req.scopes)
     res.send(200, productCategories)
     next()
   } catch (error) {
@@ -24,8 +24,13 @@ async function getAll(req, res, next) {
 }
 async function getById(req, res, next) {
   try {
-    const productCategory = await productService.getById(req.id, req.scopes)
-    res.send(200, productCategory)
+    const productCategory = await categoryService.getById(req.id, req.scopes)
+    if (productCategory) {
+      res.send(200, productCategory)
+    } else {
+      return next(new NotFoundError())
+    }
+
     next()
   } catch (error) {
     next(error)
@@ -34,7 +39,7 @@ async function getById(req, res, next) {
 
 async function update(req, res, next) {
   try {
-    await productService.update(req.id, req.body)
+    await categoryService.update(req.id, req.body)
     res.send(200)
   } catch (error) {
     next(error)
@@ -46,7 +51,7 @@ async function create(req, res, next) {
     if (req.body.id) {
       next(new BadRequestError(noId))
     } else {
-      await productService.create(req.body)
+      await categoryService.create(req.body)
       res.send(200)
       next()
     }
@@ -56,7 +61,7 @@ async function create(req, res, next) {
 }
 async function remove(req, res, next) {
   try {
-    await productService.remove(req.id)
+    await categoryService.remove(req.id)
     res.send(204)
     next()
   } catch (error) {
